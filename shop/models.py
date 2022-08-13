7d9e542a01c8d8b65f46c128b116eea5b53c8a4d
maks <max@bk.ru>
2022-08-13 15:20:11 +0300

    sql запросы

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
 
 
     class Meta:
-        ordering = 'name'
+        ordering = ('name',)
         verbose_name = 'Категория'
         verbose_name_plural = 'Категирии'
 
@@ -14,10 +14,10 @@ class Category(models.Model):
        return self.name
 
 
-class Produkt(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE())
-    name = models.CharField(max_length=100, db_column=True)
-    slug = models.CharField(max_length=100, db_column=True, unique=True)
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.CharField(max_length=10, db_index=True, unique=True)
     image = models.ImageField('products/%y/%n/%b',blank=True)
     description = models.TextField(max_length=1000, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
