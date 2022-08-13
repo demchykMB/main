d85e1363cee0693da0a5126f44a9202ab7512c9e
maks <max@bk.ru>
2022-08-13 19:00:25 +0300

    sql запросы

--- shop/models.py
+++ shop/models.py
@@ -1,39 +1,35 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, db_column=True)
-    slug = models.SlugField(max_length=100, unique=True)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField('slug', max_length=100, )
 
     class Meta:
         ordering = ('name',)
         verbose_name = 'Категория'
-        verbose_name_plural = 'Категирии'
-
-    def __str__(self):
-       return self.name
-
-
-class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.CharField(max_length=10, db_index=True, unique=True)
-    image = models.ImageField('products/%y/%n/%b',blank=True)
-    description = models.TextField(max_length=1000, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    availabel = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    uploaded = models.DateTimeField(auto_now=True)
-
-
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'Товар'
-        verbose_name_plural= 'Товары'
-        index_together= (('id', 'slug'),)
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 
+class Product(models.Model):
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.CharField(max_length=150, db_index=True, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    uploaded = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
