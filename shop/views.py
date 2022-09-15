4cd014afc559cc397eaf328ea844f520733a47d7
maks <max@bk.ru>
2022-09-15 15:49:01 +0300

    0003

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,6 @@ from . models import Product
 def index(request):
     stat = Product.objects.all()
     return render(request , 'main/index.html', {'stat':stat})
+
+def base(request):
+    render(request, "main/base.html")
