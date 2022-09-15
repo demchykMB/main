f877e08018308f6fc74890c592d439be4c08ab57
maks <max@bk.ru>
2022-09-15 21:02:40 +0300

    Вывод фото на сайт

--- main/settings.py
+++ main/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
 
 
 # Default primary key field type
