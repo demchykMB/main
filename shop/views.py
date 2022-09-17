6a092fe483f031ceded7557ee2494a3f6448c783
maks <max@bk.ru>
2022-09-17 20:42:51 +0300

    item шаблон

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,7 @@ from . models import Product
 
 def index(request):
     stat = Product.objects.all()
-    return render(request , 'main/index.html', {'stat':stat})
+    return render(request , 'HTML/index.html', {'stat':stat})
 
-def base(request):
-    render(request, "main/base.html")
+# def base(request):
+#     render(request, "main/base.html")
