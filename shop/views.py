726dbc0e6f23b453033e554b47c2921f9cd27b74
Maksim <max.demchik30@gmail.com>
2022-10-16 17:49:23 +0200

    Исправление ошибки

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login
+
 
 
 def index(request):
     stat = Product.objects.all()
     return render(request, 'HTML/index.html', {'stat': stat})
+
+
+def shop(request):
+
+    return render(request, 'HTML/shop.html')
 
 
 
